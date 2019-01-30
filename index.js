d17ea22c3cb3842be679560d562868088a00041d
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-01-30 00:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,24 +17,24 @@
 //   }
 // })
 
-let divs = document.querySelectorAll('div')
+// let divs = document.querySelectorAll('div')
  
-function bubble(e) {
+// function bubble(e) {
   
-  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
-}
+//   console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+// }
  
-for (const aDiv of divs) {
-  aDiv.addEventListener('click', bubble);
-}
+// for (const aDiv of divs) {
+//   aDiv.addEventListener('click', bubble);
+// }
 
-divs = document.querySelectorAll('div')
+// divs = document.querySelectorAll('div')
  
-function capture(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' captured')
-}
+// function capture(e) {
+//   console.log(this.firstChild.nodeValue.trim() + ' captured')
+// }
  
-for (const aDiv of divs) {
-  // set the third argument to `true`!
-  aDiv.addEventListener('click', capture, true)
-}
+// for (const aDiv of divs) {
+//   // set the third argument to `true`!
+//   aDiv.addEventListener('click', capture, true)
+// }
