96770dc2771b06a1a68932729c4c26623704eb7b
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-01-30 00:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const main = document.getElementById('main')
 main.addEventListener('click', function(event) {
   alert('I was clicked!')
 })
+
+
+const input = document.querySelector('input')
+input.addEventListener('keydown', e => console.log(e.which))
